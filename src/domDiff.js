b663505b99adbe8a2b1f6b49de6664e269c777ff
userZheng686 <1511019025@qq,com>
2021-09-13 10:37:06 +0800

    dom diff实现

---
+++ src/domDiff.js
@@ -0,0 +1,75 @@
+import { ATTR, TEXT, REPLACE, REMOVE } from "./patchTypes";
+
+let patches = {},
+    vnIndex = 0;
+
+function domDiff(oldVDom, newVDom) {
+  let index = 0;
+  vnodeWalk(oldVDom, newVDom, index);
+  return patches;
+}
+
+function vnodeWalk(oldNode, newNode, index) {
+  let vnPatch = [];
+
+  if (!newNode) {
+    vnPatch.push({
+      type: REMOVE,
+      index,
+    });
+  } else if (typeof oldNode === "string" && typeof newNode === "string") {
+    if (oldNode !== newNode) {
+      vnPatch.push({
+        type: TEXT,
+        text: newNode,
+      });
+    }
+  } else if (oldNode.type === newNode.type) {
+    const attrPatch = attrsWalk(oldNode.props, newNode.props);
+    
+    if(Object.keys(attrPatch).length > 0){
+        vnPatch.push({
+            type : ATTR,
+            atts : attrPatch
+        })
+    }
+
+    childrenWalk(oldNode.children,newNode.children)
+  } else {
+    vnPatch.push({
+        type : REPLACE,
+        newNode
+    })
+  }
+
+  if(vnPatch.length > 0){
+    patches[index] = vnPatch
+  }
+}
+
+function attrsWalk(oldAttrs, newAttrs) {
+    let attrPatch = {};
+
+    for(let key in oldAttrs){
+        // 修改属性
+        if(oldAttrs[key] !== newAttrs[key]){
+            attrPatch[key] = newAttrs[key]
+        }
+    }
+
+    for(let key in newAttrs){
+        if(!oldAttrs.hasOwnProperty(key)){
+            attrPatch[key] = newAttrs[key]
+        }
+    }
+
+    return attrPatch
+}
+
+function childrenWalk(oldChildren,newChildren){
+    oldChildren.map((c,idx) => {
+        vnodeWalk(c,newChildren[idx],++vnIndex)
+    })
+}
+
+export default domDiff;
